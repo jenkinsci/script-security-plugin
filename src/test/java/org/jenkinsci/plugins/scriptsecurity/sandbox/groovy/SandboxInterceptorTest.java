45f6ad3caa5e4fb0b9ce7dfd4bf0d1ab1f487a57
Jesse Glick <jglick@cloudbees.com>
2015-08-04 16:13:57 -0400

    [JENKINS-29541] Reproduced problem in test.

--- src/test/java/org/jenkinsci/plugins/scriptsecurity/sandbox/groovy/SandboxInterceptorTest.java
+++ src/test/java/org/jenkinsci/plugins/scriptsecurity/sandbox/groovy/SandboxInterceptorTest.java
@@ -88,6 +88,13 @@ public class SandboxInterceptorTest {
         assertEvaluate(new StaticWhitelist("new " + clazz, "method " + clazz + " specialize java.lang.Object", "method " + clazz + " quote java.lang.Object"), expected, script);
     }
 
+    @Ignore("TODO second line fails with: Scripts not permitted to use method groovy.lang.GroovyObject invokeMethod java.lang.String java.lang.Object (org.codehaus.groovy.runtime.GStringImpl substring java.lang.Integer java.lang.Integer)")
+    @Issue("JENKINS-29541")
+    @Test public void substringGString() throws Exception {
+        assertEvaluate(new GenericWhitelist(), "hell", "'hello world'.substring(0, 4)");
+        assertEvaluate(new GenericWhitelist(), "hell", "def place = 'world'; \"hello ${place}\".substring(0, 4)");
+    }
+
     /**
      * Tests the proper interception of builder-like method.
      */
