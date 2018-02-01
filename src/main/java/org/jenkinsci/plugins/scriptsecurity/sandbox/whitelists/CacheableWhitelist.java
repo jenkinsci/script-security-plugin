d6462507dc18093f8e778751a4e1e025d602db91
Sam Van Oort <samvanoort@gmail.com>
2018-02-01 14:51:07 -0500

    Make the requirements for CacheableWhitelist even more explicit

--- src/main/java/org/jenkinsci/plugins/scriptsecurity/sandbox/whitelists/CacheableWhitelist.java
+++ src/main/java/org/jenkinsci/plugins/scriptsecurity/sandbox/whitelists/CacheableWhitelist.java
@@ -2,7 +2,8 @@ package org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists;
 
 /**
  * Marker interface indicating results of permission checks do not change unless the list of entries is modified
- *  and depend ONLY on the Field/Method/Constructor actually being invoked.
+ *  and depend ONLY on the Field/Method/Constructor actually being invoked, irrespective of classloader.
+ *  That means you must be able to represent the WhiteList entry with a String signature of className + method/field signature.
  * @author Sam Van Oort
  */
 public interface CacheableWhitelist {
