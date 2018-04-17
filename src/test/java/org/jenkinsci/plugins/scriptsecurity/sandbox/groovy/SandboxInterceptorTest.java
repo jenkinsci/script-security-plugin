b841fc0c09545c3ab2662a69e960611347cbe9af
Rudolf-Walter Kiss-Szakacs <rudolf.wg@gmail.com>
2018-04-17 20:15:21 +0300

    Add test cases showing calls with parameters

--- src/test/java/org/jenkinsci/plugins/scriptsecurity/sandbox/groovy/SandboxInterceptorTest.java
+++ src/test/java/org/jenkinsci/plugins/scriptsecurity/sandbox/groovy/SandboxInterceptorTest.java
@@ -1085,5 +1085,8 @@ public class SandboxInterceptorTest {
     @Test
     public void callClosureElementOfMapAsMethod() throws Exception {
         assertEvaluate(new GenericWhitelist(), "hello", "def m = [ f: {return 'hello'} ]; m.f()");
+        assertEvaluate(new GenericWhitelist(), 15, "def m = [ f: {a -> return a*3} ]; m.f(5)");
+        assertEvaluate(new GenericWhitelist(), "a=hello,b=10", "def m = [ f: {a,b -> return \"a=${a},b=${b}\"} ]; m.f('hello',10)");
+        assertEvaluate(new GenericWhitelist(), 2, "def m = [ f: {it.size()} ]; m.f(foo:0, bar:1)");
     }
 }
