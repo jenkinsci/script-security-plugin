6475d2f0cf1fb8d07298e6f59cdf39a17f929871
Ing. Pavel Janousek <pjanouse@redhat.com>
2019-05-09 15:34:42 +0200

    Added export test Refactored as requested

--- src/test/java/org/jenkinsci/plugins/scriptsecurity/scripts/JcascTest.java
+++ src/test/java/org/jenkinsci/plugins/scriptsecurity/scripts/JcascTest.java
@@ -1,9 +1,13 @@
 package org.jenkinsci.plugins.scriptsecurity.scripts;
 
+import io.jenkins.plugins.casc.ConfigurationContext;
+import io.jenkins.plugins.casc.ConfiguratorRegistry;
 import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
 
-import org.junit.Rule;
+import io.jenkins.plugins.casc.misc.Util;
+import io.jenkins.plugins.casc.model.CNode;
+import org.junit.ClassRule;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,21 +15,24 @@ import static org.junit.Assert.assertTrue;
 
 public class JcascTest {
 
-    @Rule
-    public JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
+    @ClassRule
+    @ConfiguredWithCode("smoke_test.yaml")
+    public static JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
 
     @Test
-    @ConfiguredWithCode("smoke-test-empty.yaml")
-    public void smokeTestEmpty() throws Exception {
-        String[] approved = ScriptApproval.get().getApprovedSignatures();
-        assertTrue(approved.length == 0);
-    }
-
-    @Test
-    @ConfiguredWithCode("smoke-test-entry.yaml")
     public void smokeTestEntry() throws Exception {
         String[] approved = ScriptApproval.get().getApprovedSignatures();
         assertTrue(approved.length == 1);
         assertEquals(approved[0], "method java.net.URI getHost");
+    }
+
+    @Test
+    public void smokeTestExport() throws Exception {
+        ConfiguratorRegistry registry = ConfiguratorRegistry.get();
+        ConfigurationContext context = new ConfigurationContext(registry);
+        CNode yourAttribute = Util.getUnclassifiedRoot(context).get("scriptApproval");
+        String exported = Util.toYamlString(yourAttribute);
+        String expected = Util.toStringFromYamlFile(this, "smoke_test_expected.yaml");
+        assertEquals(exported, expected);
     }
 }
