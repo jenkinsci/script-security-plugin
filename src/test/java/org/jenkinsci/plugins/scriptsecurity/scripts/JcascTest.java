3b77f808a71620a526a59a2e625d289b4c249234
Pavel Janousek <pjanouse@redhat.com>
2019-09-13 20:13:38 +0200

    [JENKINS-57563] - Added JCasC to ScriptApproval (#250)
    Co-authored-by: Daniel Garzon <d3@digitalasset.com>
    Co-authored-by: Ing. Pavel Janousek <pjanouse@redhat.com>
    Co-authored-by: rsandell <rsandell@cloudbees.com>

---
+++ src/test/java/org/jenkinsci/plugins/scriptsecurity/scripts/JcascTest.java
@@ -0,0 +1,40 @@
+package org.jenkinsci.plugins.scriptsecurity.scripts;
+
+import io.jenkins.plugins.casc.ConfigurationContext;
+import io.jenkins.plugins.casc.ConfiguratorRegistry;
+import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+
+import io.jenkins.plugins.casc.model.CNode;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import static io.jenkins.plugins.casc.misc.Util.getSecurityRoot;
+import static io.jenkins.plugins.casc.misc.Util.toStringFromYamlFile;
+import static io.jenkins.plugins.casc.misc.Util.toYamlString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class JcascTest {
+
+    @ClassRule
+    @ConfiguredWithCode("smoke_test.yaml")
+    public static JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
+
+    @Test
+    public void smokeTestEntry() throws Exception {
+        String[] approved = ScriptApproval.get().getApprovedSignatures();
+        assertTrue(approved.length == 1);
+        assertEquals(approved[0], "method java.net.URI getHost");
+    }
+
+    @Test
+    public void smokeTestExport() throws Exception {
+        ConfiguratorRegistry registry = ConfiguratorRegistry.get();
+        ConfigurationContext context = new ConfigurationContext(registry);
+        CNode yourAttribute = getSecurityRoot(context).get("scriptApproval");
+        String exported = toYamlString(yourAttribute);
+        String expected = toStringFromYamlFile(this, "smoke_test_expected.yaml");
+        assertEquals(exported, expected);
+    }
+}
