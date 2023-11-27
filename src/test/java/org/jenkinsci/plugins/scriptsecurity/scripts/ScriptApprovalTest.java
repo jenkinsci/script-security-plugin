2e73aa7a07cf7c4980c827bc43e770bc7a4b5714
Jan Meiswinkel <meiswinkel.jan@googlemail.com>
2023-11-27 15:11:59 +0100

    Update ScriptApprovalTest.java

--- src/test/java/org/jenkinsci/plugins/scriptsecurity/scripts/ScriptApprovalTest.java
+++ src/test/java/org/jenkinsci/plugins/scriptsecurity/scripts/ScriptApprovalTest.java
@@ -83,7 +83,7 @@ public class ScriptApprovalTest extends AbstractApprovalTest<ScriptApprovalTest.
     }
 
     @Test
-    public void reapprovingShouldFail() throws Exception {
+    public void afterDenyScriptShouldBePending() throws Exception {
         configureSecurity();
         final ScriptApproval sa = ScriptApproval.get();
         FreeStyleProject p = r.createFreeStyleProject();
