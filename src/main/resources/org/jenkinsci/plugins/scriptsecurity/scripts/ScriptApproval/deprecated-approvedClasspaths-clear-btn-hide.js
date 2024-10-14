4778ca84bde5981deaea798db9d174bf0b93a7b0
Basil Crow <me@basilcrow.com>
2024-10-14 07:36:13 -0700

    [JENKINS-69651] CSP compatibility for `ScriptApproval` (#582)

---
+++ src/main/resources/org/jenkinsci/plugins/scriptsecurity/scripts/ScriptApproval/deprecated-approvedClasspaths-clear-btn-hide.js
@@ -0,0 +1,2 @@
+document.getElementById('deprecated-approvedClasspaths-clear-btn').style.display = 'none';
+document.getElementById('deprecated-approvedClasspaths-clear-spinner').style.display = '';
---
+++ src/main/resources/org/jenkinsci/plugins/scriptsecurity/scripts/ScriptApproval/deprecated-approvedClasspaths-clear-btn-show.js
@@ -0,0 +1,2 @@
+document.getElementById('deprecated-approvedClasspaths-clear-btn').style.display = '';
+document.getElementById('deprecated-approvedClasspaths-clear-spinner').style.display = 'none';
