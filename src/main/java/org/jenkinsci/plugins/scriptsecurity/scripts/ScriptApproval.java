9af9b3ac253d90979bd6a3eec6e979d04ca2b5d1
Guruprasad Bhat <gbhat@cloudbees.com>
2026-08-25 15:23:51 +0200

    SECURITY-3986

--- src/main/java/org/jenkinsci/plugins/scriptsecurity/scripts/ScriptApproval.java
+++ src/main/java/org/jenkinsci/plugins/scriptsecurity/scripts/ScriptApproval.java
@@ -522,7 +522,6 @@ public final class ScriptApproval extends GlobalConfiguration implements RootAct
         }
     }
 
-    @DataBoundConstructor
     public ScriptApproval() {
         load();
     }
